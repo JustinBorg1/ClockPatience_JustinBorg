3bf0242d9c3f5775c6e3444e56d623afdbd84bbf
JustinBorg1 <justin.borg.e21729@mcast.edu.mt>
2023-02-03 21:21:35 +0100

    Added counters and outputs

--- ClockPatience_JB/Program.cs
+++ ClockPatience_JB/Program.cs
@@ -7,25 +7,219 @@ public class Program
 {
     public static void Main()
     {
-        int c = 0;
+        var channels = new int[13];
+        channels[0] = 0;    //A
+        channels[1] = 0;    //2
+        channels[2] = 0;    //3
+        channels[3] = 0;    //4
+        channels[4] = 0;    //5
+        channels[5] = 0;    //6
+        channels[6] = 0;    //7
+        channels[7] = 0;    //8
+        channels[8] = 0;    //9
+        channels[9] = 0;    //T
+        channels[10] = 0;   //J
+        channels[11] = 0;   //Q
+        channels[12] = 0;   //K
+
+
         DeckOfCards deck;
         deck = new DeckOfCards();
         deck.shuffle(100);
 
 
-        for (int i = 0; i < 52; i++)
-        {
-            Card newCard = deck.deal();
-            Console.WriteLine(newCard.toString());
+        Console.WriteLine("Choose a specific rank and you will be outputed with the amount of cards that had to be dealt before all of your rank's cards were collected.");
 
-            if(newCard.toString().Contains("2"))
+
+        Console.WriteLine("Enter Desired Rank to Collect:");
+        string userInp = Console.ReadLine().ToUpper();
+
+
+        if (userInp == "A" || userInp == "2" || userInp == "3" || userInp == "4" || userInp == "5" || userInp == "6" || userInp == "7" || userInp == "8" || userInp == "9" || userInp == "T" || userInp == "J" || userInp == "Q" || userInp == "K")
+        {
+
+            for (int i = 0; i < 52; i++)
             {
-                c++;
+
+                Card newCard = deck.deal();
+                Console.Write(newCard.toString());
+
+                //add space between each card
+                Console.Write(" ");
+
+                //move to a new line every 13 cards
+                if ((i + 1) % 13 == 0)
+                {
+                    Console.WriteLine();
+                }
+
+
+                //card counter for each channel
+                if (newCard.toString().Contains("A") && channels[0] != 4 && userInp == "A")
+                {
+                    channels[0]++;
+                }
+                else if (channels[0] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("A rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("2") && channels[1] != 4 && userInp == "2")
+                {
+                    channels[1]++;
+                }
+                else if (channels[1] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("1 rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("3") && channels[2] != 4 && userInp == "3")
+                {
+                    channels[2]++;
+                }
+                else if (channels[2] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("2 rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("4") && channels[3] != 4 && userInp == "4")
+                {
+                    channels[3]++;
+                }
+                else if (channels[3] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("4 rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("5") && channels[4] != 4 && userInp == "5")
+                {
+                    channels[4]++;
+                }
+                else if (channels[4] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("5 rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("6") && channels[5] != 4 && userInp == "6")
+                {
+                    channels[5]++;
+                }
+                else if (channels[5] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("6 rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("7") && channels[6] != 4 && userInp == "7")
+                {
+                    channels[6]++;
+                }
+                else if (channels[6] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("7 rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("8") && channels[7] != 4 && userInp == "8")
+                {
+                    channels[7]++;
+                }
+                else if (channels[7] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("8 rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("9") && channels[8] != 4 && userInp == "9")
+                {
+                    channels[8]++;
+                }
+                else if (channels[8] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("9 rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("T") && channels[9] != 4 && userInp == "T")
+                {
+                    channels[9]++;
+                }
+                else if (channels[9] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("T rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("J") && channels[10] != 4 && userInp == "J")
+                {
+                    channels[10]++;
+                }
+                else if (channels[10] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("J rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("Q") && channels[11] != 4 && userInp == "Q")
+                {
+                    channels[11]++;
+                }
+                else if (channels[11] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Q rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
+
+                if (newCard.toString().Contains("K") && channels[12] != 4 && userInp == "K")
+                {
+                    channels[12]++;
+                }
+                else if (channels[12] == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("K rank fully collected after " + i.ToString() + " cards.");
+                    break;
+                }
+
             }
+
         }
-        Console.WriteLine(c);
+        else
+        {
+            Console.WriteLine("Incorrect Input");
+        }
 
     }
+
 
     public static string PadNumbers(string input)
     {
